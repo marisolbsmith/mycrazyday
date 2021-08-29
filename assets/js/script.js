7f65c2d13cc7fe9a339eab61b4a1da2bee38764b
mrssmith <marisolbsmith@yahoo.com>
2021-08-28 19:39:59 -0500

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
+function saveEvent(id) {
+  let eventMessage = $(`textarea[hourid="${id}"]`).val();
+  window.localStorage.setItem(`event_${id}`, eventMessage);
+}
+
 var todaysDate = moment().format("MM DD YYYY");
 console.log(todaysDate);
 $("#currentDay").text(todaysDate);
@@ -7,13 +12,17 @@ let currentHour = moment().hour();
 for (i = 8; i < 18; i++) {
   let hour = i % 12 == 0 ? 12 : i % 12;
   let currentClass = "present";
+  let eventMessage = window.localStorage.getItem(`event_${i}`);
+  eventMessage = eventMessage == null ? "" : eventMessage;
 
   if (currentHour > i) {
     currentClass = "future";
   } else if (currentHour < i) {
     currentClass = "past";
   }
-  $(`<div class="row ${currentClass}">
-    ${hour}
+  $(`<div class="row">
+    <div class="hour ">${hour}</div> 
+    <textarea hourid="${i}" class="${currentClass}">${eventMessage}</textarea>
+    <button type="button" onclick="saveEvent(${i})" class= "saveBtn">save</button>
     </div>`).appendTo(`.container`);
 }
